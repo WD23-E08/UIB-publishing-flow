6a0b7ca44c51d8ffa5b366ccaa7f3522b584ee8e
ShiarOs <shiarsham3@gmail.com>
2022-09-07 15:46:54 +0200

    test1

---
+++ __tests__/task.1.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+let browser;
+let page;
+
+
+
+const { promisify } = require('util')
+const { exec } = require('child_process');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+
+const asyncExec = promisify(exec);
+
+async function runCommand(command) {
+  const out = await asyncExec(command)
+  return out.stdout.replace(/(\r\n|\n|\r)/gm, "") // remove line breaks
+}
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
+    await page.goto('file://' + path.join(__dirname, '../index.html'));
+}, 30000);
+
+afterAll((done) => {
+    try {
+        this.puppeteer.close();
+    } catch (e) { }
+    done();
+});
+
+
+describe('test', () => {
+    test("command", async () => {
+        const pattern = new RegExp(/(?:git@|https:\/\/)github.com[:/](((?!\.git).)*)/)
+        const result = await runCommand(`git config --get remote.origin.url`)
+        expect(result).toBeDefined()
+        const match = result.match(pattern)
+        expect(match).toEqual(expect.arrayContaining([expect.anything()]))
+        const [ owner, repo ] = match[1].split("/")
+        const response = await page.goto(`https://${owner}.github.io/${repo}`)
+        
+        expect(response.status()).toBeLessThan(400)
+    });
+});
